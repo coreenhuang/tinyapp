1e784167663c02aebccdcef39f86fd626826decb
Coreen Huang <coreen.huang@gmail.com>
2022-09-06 08:52:43 -0400

    urls_new file for form submission

--- express_server.js
+++ express_server.js
@@ -8,6 +8,10 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+// The body-parser library will convert the request body from a Buffer into string that we can read
+
+app.use(express.urlencoded({ extended: true }));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -16,6 +20,10 @@ const urlDatabase = {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
