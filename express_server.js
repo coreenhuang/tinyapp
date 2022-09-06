2057913ca4894fc3110b8e68f80106316129a275
Coreen Huang <coreen.huang@gmail.com>
2022-09-06 09:12:19 -0400

    post route to receive form submission and egenerating random string

--- express_server.js
+++ express_server.js
@@ -17,9 +17,16 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {}
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/new", (req, res) => {
