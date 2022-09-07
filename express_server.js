3e2508b3738c912f049948e80961e182141ba5de
Coreen Huang <coreen.huang@gmail.com>
2022-09-07 14:50:01 -0400

    added form element to add delete button and POST route for /urls/:id/delete to remove URLS

--- express_server.js
+++ express_server.js
@@ -44,6 +44,12 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${newID}`);
 });
 
+app.post("/urls/:id/delete", (req, res) => {
+  const {id} = req.params;
+  delete urlDatabase[id];
+  res.redirect("/urls");
+})
+
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
