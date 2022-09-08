fe6422f7854ac449e0befc7593efc81d7d29e228
Coreen Huang <coreen.huang@gmail.com>
2022-09-08 19:53:04 -0400

    added cookies for log in, clear cookies for logout

--- express_server.js
+++ express_server.js
@@ -1,16 +1,25 @@
-// Following code allows us to make HTTP requests on port 8080
-
 const express = require("express");
-const app = express();
-const PORT = 8080; // default port 8080
+const cookieParser = require('cookie-parser');
 
-// This tells the Express app to use EJS as its templating engine
+const app = express();
+app.use(cookieParser());
+const PORT = 8080; 
 
 app.set("view engine", "ejs");
-
-// The body-parser library will convert the request body from a Buffer into string that we can read
-
 app.use(express.urlencoded({ extended: true }));
+
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -30,18 +39,53 @@ function generateRandomString() {
   return newID;
 }
 
+//index page of urls
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const username = req.cookies.username;
+  const templateVars = { urls: urlDatabase, username};
   res.render("urls_index", templateVars);
 });
 
+//adding to url list
 app.post("/urls", (req, res) => {
 
-  let newID = generateRandomString();
+  let newShortURLID = generateRandomString();
   // adds to database, generating random id for key, and adds the client's long URL input as the value
-  urlDatabase[newID] = req.body.longURL;
+  urlDatabase[newShortURLID] = req.body.longURL;
 
-  res.redirect(`/urls/${newID}`);
+  res.redirect(`/urls/${newShortURLID}`);
+});
+
+//page to registration page
+app.get("/register", (req, res) => {
+  const username = req.cookies.username;
+  res.render("urls_register", {username});
+});
+
+//registering username and password
+app.post("/register", (req, res) => {
+
+  let newUserID = generateRandomString();
+
+  users[newUserID] = {
+    id: newUserID,
+    email: req.body.email,
+    password: req.body.password
+  }
+
+  res.redirect("/urls");
+});
+
+//logging in
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username); 
+  res.redirect("/urls");
+});
+
+//logging out
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
 });
 
 // edit urls
@@ -50,14 +94,14 @@ app.post("/urls/:id", (req, res) => {
   const newLongURL = req.body.longURL;
   urlDatabase[id] = newLongURL;
   res.redirect("/urls");
-})
+});
 
 //delete urls
 app.post("/urls/:id/delete", (req, res) => {
   const {id} = req.params;
   delete urlDatabase[id];
   res.redirect("/urls");
-})
+});
 
 //use short url to go to actual long urls
 app.get("/u/:id", (req, res) => {
@@ -67,12 +111,14 @@ app.get("/u/:id", (req, res) => {
 
 //page to make new urls
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const username = req.cookies.username;
+  res.render("urls_new", {username});
 });
 
 // page to show output of new url input
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const username = req.cookies.username;
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username };
   res.render("urls_show", templateVars);
 });
 
@@ -80,23 +126,17 @@ app.get("/urls/:id", (req, res) => {
 
 
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-
-
-
-
-
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
 
 app.listen(PORT, () => {
